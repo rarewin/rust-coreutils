c7c6cdc2db1ca9588a3a2c886d59bd21d1114fda
Katsuki Kobayashi <rare@tirasweel.org>
2018-08-09 22:46:05 +0900

    implement echo command which has only '-n' option

--- echo/src/main.rs
+++ echo/src/main.rs
@@ -1,3 +1,28 @@
+extern crate clap;
+
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let m = App::new("echo")
+        .arg(Arg::with_name("STRING").multiple(true))
+        .arg(
+            Arg::with_name("n")
+                .short("n")
+                .help("do not output the trailing newline"),
+        ).get_matches();
+
+    if m.occurrences_of("STRING") > 0 {
+        let args: Vec<_> = m.values_of("STRING").unwrap().collect();
+
+        for i in 0..args.len() {
+            print!("{}", args[i]);
+            if i < args.len() - 1 {
+                print!(" ");
+            }
+        }
+    }
+
+    if !m.is_present("n") {
+        println!("");
+    }
 }
